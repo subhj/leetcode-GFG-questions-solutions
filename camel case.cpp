b8f81e371d7a3a1d558e6037386bbbaf0ace9824
sachin jeena <98201527+subhj@users.noreply.github.com>
2023-03-20 19:33:32 +0530

    hackerearth

---
+++ camel case.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+ * Complete the 'camelcase' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING s as parameter.
+ */
+
+int camelcase(string s) {
+    
+    int numberofstring=0;
+    
+    for(int i=0; s[i] != '\0'; i++){
+        
+        
+        if(s[i] >= 'A' && s[i]<= 'Z'){
+            
+            numberofstring++;
+        }
+        }
+         numberofstring = numberofstring +1;
+        
+        return numberofstring;
+    }
+
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    int result = camelcase(s);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
